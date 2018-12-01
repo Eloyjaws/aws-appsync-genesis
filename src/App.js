73767fca98a3d69aa93880b53f6aca93f7593a89
eajiboye <eajiboye@inlaks.com>
2018-12-01 15:12:33 +0100

    8/9

--- src/App.js
+++ src/App.js
@@ -1,28 +1,91 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { graphql, compose } from "react-apollo";
+import { graphqlMutation } from "aws-appsync-react";
+import { buildSubscription } from "aws-appsync";
+import gql from "graphql-tag";
+import "./styles.css";
+
+const CreateTodo = gql`
+  mutation($title: String!, $completed: Boolean!) {
+    createTodo(input: { title: $title, completed: $completed }) {
+      id
+      title
+      completed
+    }
+  }
+`;
+
+const ListTodos = gql`
+  query {
+    listTodos {
+      items {
+        id
+        title
+        completed
+      }
+    }
+  }
+`;
+
+const SubscribeToTodos = gql`
+  subscription onCreateTodo {
+    onCreateTodo {
+      id
+      title
+      completed
+    }
+  }
+`;
 
 class App extends Component {
+  state = {
+    todo: ""
+  };
+
+  componentDidMount() {
+    this.props.data.subscribeToMore(
+      buildSubscription(SubscribeToTodos, ListTodos)
+    );
+  }
+
+  addTodo = () => {
+    if (this.state.todo === "") return;
+    const todo = {
+      title: this.state.todo,
+      completed: false
+    };
+    this.props.createTodo(todo);
+    this.setState({ todo: "" });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h2>Add a TODO item</h2>
+          <input onChange={e => this.setState({ todo: e.target.value })} />
+          <button onClick={this.addTodo}>Add Todo</button>
+          {this.props.todos.map(({ title }, i) => (
+            <p key={i}>
+              {i} - {title}
+            </p>
+          ))}
         </header>
       </div>
     );
   }
 }
 
-export default App;
+export default compose(
+  graphqlMutation(CreateTodo, ListTodos, "Todo"),
+  graphql(ListTodos, {
+    options: {
+      fetchPolicy: "cache-and-network"
+    },
+    props: props => ({
+      subscribeToMore: props.data.subscribeToMore,
+      todos: props.data.listTodos ? props.data.listTodos.items : [],
+      data: props.data
+    })
+  })
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import Client from "aws-appsync";
+import { ApolloProvider } from "react-apollo";
+import { Rehydrated } from "aws-appsync-react";
+import { AWSAppSyncClient } from "aws-appsync";
+import AppSync from "./AppSync";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new AWSAppSyncClient({
+  url: AppSync.graphqlEndpoint,
+  region: AppSync.region,
+  auth: {
+    type: AppSync.authenticationType,
+    apiKey: AppSync.apiKey
+  }
+});
+
+const WithProvider = () => (
+  <ApolloProvider client={client}>
+    <Rehydrated>
+      <App />
+    </Rehydrated>
+  </ApolloProvider>
+);
+
+ReactDOM.render(<WithProvider />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
